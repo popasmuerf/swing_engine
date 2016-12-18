33480ccb6893887cba5aae05658a6a1827562bf1
adjunct_black <MichaelBrown172@Gmail.com>
2016-12-18 18:11:53 -0500

    ....

--- src/main/java/com/thirdplanet/s_engine/two_d/GamePanel.java
+++ src/main/java/com/thirdplanet/s_engine/two_d/GamePanel.java
@@ -1,3 +1,6 @@
+http://fivedots.coe.psu.ac.th/~ad/jg/ch1/index.html
+import javax.swing.* ;
+
 public class GamePanel extends JPanel implements Runnable{
         private static final int PWIDTH = 500 ;
         private static final int PHEIGHT = 400 ;
@@ -5,6 +8,10 @@ public class GamePanel extends JPanel implements Runnable{
         private Thread animator ;
         private Thread boolean running = false ;
         private boolean gameOver = false ;
+
+        private Graphics dbg ;
+        private Image dbImage = null ;
+
         public GamePanel(){
                 setBackground(Color.white) ;
                 setPreferredSize(new Dimension(PWIDTH,PHEIGHT) ;
@@ -21,6 +28,29 @@ public class GamePanel extends JPanel implements Runnable{
         }
         public void stopGame(){
                 running = false ;
+        }
+        private void gameOverMessage(Graphics g){
+                g.drawString(msg,x,y);
+        }
+        public void paintComponent(Graphics g){
+                super.paintComponent(g);
+                if(dbImage!=null){
+                        g.drawImage(dbImage,0,0, null);
+                }
+        }
+        public void gameRender(){
+                if(dbImage == null){
+                        System.out.println("dbImage is null") ;
+                        return ;
+                }else{
+                        dbg = dbImage.getGraphics();
+                        dbg.setColor(Color.white);
+                        dbg.fillRect(0,0,PWIDTH,PHEIGHT);
+                        //draw game elements
+                        if(gameOver){
+                                gameOverMessage(dbg) ;
+                        }
+                }
         }
         public void run(){
                 running =  true ;
